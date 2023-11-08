f6fed75b97fdf2e919e1f02701be9150a4e78ac0
Keziah Le Roux <122865752+SlaveToPurpose@users.noreply.github.com>
2023-11-08 12:59:37 +0000

    args, kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,14 +8,23 @@ from datetime import datetime
 class BaseModel:
     """Base class for all classes"""
 
-    def __init__(self):
-        """
-        Initialise 3 public instance attributes
-        Task 3 version
-        """
-        self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+	def __init__(self, *args, **kwargs):
+		"""
+		Initialise 3 public instance attributes
+		use *args, **kwargs arguments for the constructor of a BaseModel
+		"""
+		if len(kwargs) != 0:
+			for key, value in kwargs.items():
+				if key == "created_at" or key == "updated_at":
+					newVal = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+					setattr(self, key, newVal)
+				else:
+					setattr(self, key, value)
+
+		else:
+			self.id = str(uuid.uuid4())
+			self.created_at = datetime.now()
+			self.updated_at = datetime.now()
 
     def __str__(self):
         """
