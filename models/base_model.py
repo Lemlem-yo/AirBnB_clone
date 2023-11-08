73bc64badb67ad7934c6da27c1af336c44f132f7
Keziah Le Roux <122865752+SlaveToPurpose@users.noreply.github.com>
2023-11-08 18:48:36 +0000

    test using provided test and debug

--- models/base_model.py
+++ models/base_model.py
@@ -8,23 +8,25 @@ from datetime import datetime
 class BaseModel:
     """Base class for all classes"""
 
-	def __init__(self, *args, **kwargs):
-		"""
-		Initialise 3 public instance attributes
-		use *args, **kwargs arguments for the constructor of a BaseModel
-		"""
-		if len(kwargs) != 0:
-			for key, value in kwargs.items():
-				if key == "created_at" or key == "updated_at":
-					newVal = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-					setattr(self, key, newVal)
-				else:
-					setattr(self, key, value)
+    def __init__(self, *args, **kwargs):
+        """
+        Initialise 3 public instance attributes
+        use *args, **kwargs arguments for the constructor of a BaseModel
+        """
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    newVal = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, newVal)
+                elif key == "__class__":
+                    continue
+                else:
+                    setattr(self, key, value)
 
-		else:
-			self.id = str(uuid.uuid4())
-			self.created_at = datetime.now()
-			self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
