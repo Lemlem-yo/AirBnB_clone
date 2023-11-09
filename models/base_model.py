047d567465a9435f11eca2c5e91ed3fc31f8a838
Keziah Le Roux <122865752+SlaveToPurpose@users.noreply.github.com>
2023-11-09 09:30:08 +0000

    import BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@ Base class that will be the foundation for Airbnb project
 """
 
 import uuid
+import models
 from datetime import datetime
 
 class BaseModel:
@@ -13,7 +14,7 @@ class BaseModel:
         Initialise 3 public instance attributes
         use *args, **kwargs arguments for the constructor of a BaseModel
         """
-        if len(kwargs) != 0:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     newVal = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
@@ -27,6 +28,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
+            
 
     def __str__(self):
         """
@@ -42,7 +45,7 @@ class BaseModel:
         Update the public instance attribute updated_at
         to the current datetime
         """
-        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
