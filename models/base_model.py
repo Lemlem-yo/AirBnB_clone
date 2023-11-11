a5e221a3bcd5d361b90ff14bd0b5eba2f18bd84c
Keziah Le Roux <122865752+SlaveToPurpose@users.noreply.github.com>
2023-11-11 20:29:53 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -28,8 +28,8 @@ class BaseModel:
 
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
