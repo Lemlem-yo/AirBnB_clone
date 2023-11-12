c1a6ff721c267281fda3428f3def0cbcecca191c
SlaveToPurpose <keziahleroux@gmail.com>
2023-11-12 12:36:13 -0800

    create func

--- console.py
+++ console.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """hbnb  console"""
 import cmd
+import json
+import shlex
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,6 +15,7 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = "(hbnb) "
+    file_path = "file.json"
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -24,6 +28,27 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty input line"""
         pass
+
+    def do_create(self, arg):
+        """Creates an instance of BaseModel, saves and prints it"""
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            obj = eval(arg)()
+            obj.save()
+            print(obj.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance."""
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+            return
+
+    
 
 
 if __name__ == '__main__':
